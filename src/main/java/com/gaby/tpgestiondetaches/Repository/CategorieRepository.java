4bed328a9e3773d10fe72275af72ba25c84ef1f3
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-15 15:02:18 +0400

    création du repository de Categorie

---
+++ src/main/java/com/gaby/tpgestiondetaches/Repository/CategorieRepository.java
@@ -0,0 +1,8 @@
+package com.gaby.tpgestiondetaches.Repository;
+
+import com.gaby.tpgestiondetaches.Entite.Categorie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategorieRepository extends CrudRepository<Categorie, Long> {
+
+}
