1791ffbc323514c5920d4fe66d4bde388e3a71ad
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-15 15:01:42 +0400

    création du repository de TacheSimple

---
+++ src/main/java/com/gaby/tpgestiondetaches/Repository/TacheSimpleRepository.java
@@ -0,0 +1,7 @@
+package com.gaby.tpgestiondetaches.Repository;
+
+import com.gaby.tpgestiondetaches.Entite.TacheSimple;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TacheSimpleRepository extends CrudRepository<TacheSimple, Long> {
+}
