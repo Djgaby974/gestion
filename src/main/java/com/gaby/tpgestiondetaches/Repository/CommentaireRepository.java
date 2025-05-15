a9d0e74d2e5bf279435a65f9a6a73cedf31df48d
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-15 15:01:59 +0400

    création du repository de Commentaire

---
+++ src/main/java/com/gaby/tpgestiondetaches/Repository/CommentaireRepository.java
@@ -0,0 +1,7 @@
+package com.gaby.tpgestiondetaches.Repository;
+
+import com.gaby.tpgestiondetaches.Entite.Commentaire;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentaireRepository extends CrudRepository<Commentaire, Long> {
+}
