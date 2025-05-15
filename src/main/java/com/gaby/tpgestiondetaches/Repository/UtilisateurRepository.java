1f92a6819753a85fb553aad407e932517c37e71c
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-15 15:01:22 +0400

    création du repository de Utilisateur

---
+++ src/main/java/com/gaby/tpgestiondetaches/Repository/UtilisateurRepository.java
@@ -0,0 +1,7 @@
+package com.gaby.tpgestiondetaches.Repository;
+
+import com.gaby.tpgestiondetaches.Entite.Utilisateur;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
+}
