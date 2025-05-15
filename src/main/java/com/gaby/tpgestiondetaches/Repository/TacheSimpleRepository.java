3ae609e2f6d34bd94bf44d3d7d47a267e43c993a
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-15 15:22:02 +0400

    modification du repository TacheSimple pour faire la liste des taches en cours ou terminée

--- src/main/java/com/gaby/tpgestiondetaches/Repository/TacheSimpleRepository.java
+++ src/main/java/com/gaby/tpgestiondetaches/Repository/TacheSimpleRepository.java
@@ -3,5 +3,9 @@ package com.gaby.tpgestiondetaches.Repository;
 import com.gaby.tpgestiondetaches.Entite.TacheSimple;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TacheSimpleRepository extends CrudRepository<TacheSimple, Long> {
+    List<TacheSimple> findByTermineeTrue();   // tâches terminées
+    List<TacheSimple> findByTermineeFalse();  // tâches en cours
 }
